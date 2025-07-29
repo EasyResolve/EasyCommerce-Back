4aefbe1c54b863a9281486c8ac3da1c90e334b25
88006 - Damián Díaz <101681991+88006DamianDiaz@users.noreply.github.com>
2025-07-29 14:00:33 -0300

    feat: Inicializacion de proyecto

---
+++ easycommerce/src/test/java/easycommerce/easycommerce/EasycommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package easycommerce.easycommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasycommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
