f78417b199a1cd42f3fc70039ba601e7839a0ce7
ejorussa <105822569+ejorussa@users.noreply.github.com>
2025-08-13 20:43:20 -0300

    fix: Habilitacion de schedulers

--- easycommerce/src/main/java/easycommerce/easycommerce/EasycommerceApplication.java
+++ easycommerce/src/main/java/easycommerce/easycommerce/EasycommerceApplication.java
@@ -2,8 +2,10 @@ package easycommerce.easycommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EasycommerceApplication {
 
 	public static void main(String[] args) {
